a7f09de9622d0d4ae4ffc0a64436f490b9f87f96
Randolph Jones <RandolphJones5686@outlook.com>
2021-12-27 23:56:01 +0800

    made all of the square the same seven color possibilities

--- sqrgame.py
+++ sqrgame.py
@@ -5,13 +5,12 @@ from tkinter import *
 from PIL import ImageTk, Image 
 root = Tk()
 
-hex_chars = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
-rand_color = '#' + choice(hex_chars) + choice(hex_chars)+ choice(hex_chars)+ choice(hex_chars)+ choice(hex_chars)+ choice(hex_chars) 
-my_canvas1 = Canvas(root, bg=rand_color, height= 200, width=200)
-my_canvas1.grid(row=0, column=0)
-print(rand_color)
-
 colors_written = ['orange','blue','red','purple', 'green', 'yellow', 'black']
+rand_color1 = choice(colors_written)
+my_canvas1 = Canvas(root, bg=rand_color1, height= 200, width=200)
+my_canvas1.grid(row=0, column=0)
+print(rand_color1)
+
 rand_color2 = choice(colors_written)
 my_canvas2 = Canvas(root, bg=rand_color2, height= 200, width=200)
 my_canvas2.grid(row=0, column=1)
@@ -20,9 +19,12 @@ print(rand_color2)
 rand_color3 = choice(colors_written)
 my_canvas3 = Canvas(root, bg=rand_color3, height=200, width=200)
 my_canvas3.grid(row=1, column=1)
+print(rand_color3)
 
 rand_color4 = choice(colors_written)
 my_canvas4 = Canvas(root, bg=rand_color4, height=200, width=200)
 my_canvas4.grid(row=1, column=0)
+print(rand_color4)
+
 
 root.mainloop()
