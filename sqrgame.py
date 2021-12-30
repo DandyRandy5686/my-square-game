e9f5443218151a472aefffa05b90533c3b1b4d20
Randolph Jones <RandolphJones5686@outlook.com>
2021-12-31 01:20:45 +0800

    added fuctionality to shuffle button

--- sqrgame.py
+++ sqrgame.py
@@ -5,11 +5,22 @@ from tkinter import *
 from PIL import ImageTk, Image 
 root = Tk()
 
+root.title("Sqaure Slots")
+
+def shuffle_colors():
+    my_canvas1['bg']= choice(colors_written)
+    my_canvas2['bg']= choice(colors_written)
+    my_canvas3['bg']= choice(colors_written)
+    my_canvas4['bg']= choice(colors_written)
+    print("button pushed")
+
+
+colors_written = ['orange','blue','red','purple', 'green', 'yellow', 'black']
+
 
 game_name = Label(root, bg="green", text= "Slot Squares", font="FreeMono 40",height=1, width=20)
 game_name.grid(row=0, column=0, columnspan=2)
 
-colors_written = ['orange','blue','red','purple', 'green', 'yellow', 'black']
 rand_color1 = choice(colors_written)
 my_canvas1 = Canvas(root, bg=rand_color1, height= 200, width=200)
 my_canvas1.grid(row=1, column=0)
@@ -21,8 +32,12 @@ my_canvas2.grid(row=1, column=1)
 print(rand_color2)
 
 
-shuffle_button = Button(root, bg="green", text= "Space for Button", font="FreeMono 10",height=5, width=20)
+shuffle_button = Button(root, bg="green", text= "Space for Button", command=shuffle_colors,  font="FreeMono 10",height=5, width=20)
 shuffle_button.grid(row=2, column=0, columnspan=2)
+
+#lambda : shuffle_colors()
+#This also works with the command key pair, but I don't understand it so I'll use shuffle colors.
+#I remember in other contexts, the lambda was used because otherwise we couldn't pass anything in when we ran the funciton. 
 
 
 
@@ -35,6 +50,8 @@ rand_color4 = choice(colors_written)
 my_canvas4 = Canvas(root, bg=rand_color4, height=200, width=200)
 my_canvas4.grid(row=3, column=1)
 print(rand_color4)
+
+#lambda : shuffle_colors()
 
 
 root.mainloop()
